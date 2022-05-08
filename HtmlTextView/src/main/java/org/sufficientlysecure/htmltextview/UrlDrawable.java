42e033a336d1f3a9adc42ebf2de7d606036b3d38
Jame <annabalfskiles@gmail.com>
2022-05-08 12:27:10 +0400

    init

---
+++ HtmlTextView/src/main/java/org/sufficientlysecure/htmltextview/UrlDrawable.java
@@ -0,0 +1,26 @@
+package org.sufficientlysecure.htmltextview;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created time : 2022/5/8 12:16.
+ *
+ * @author 10585
+ */
+public class UrlDrawable extends BitmapDrawable {
+    protected Drawable drawable;
+    public UrlDrawable(){
+        super(null,(Bitmap)null);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        // override the draw to facilitate refresh function later
+        if (drawable != null) {
+            drawable.draw(canvas);
+        }
+    }
+}
